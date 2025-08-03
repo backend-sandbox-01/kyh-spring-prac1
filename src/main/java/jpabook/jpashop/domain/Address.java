ca4a570299c93bba430076b0dc3ea3de96e1082c
jaemin <jaemin4033@gmail.com>
2025-08-04 03:00:56 +0900

    회원, 상품, 주문 도메인 개발

---
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -0,0 +1,23 @@
+package jpabook.jpashop.domain;
+
+import jakarta.persistence.Embeddable;
+import lombok.Getter;
+
+@Embeddable
+@Getter
+public class Address {
+
+    private String city;
+    private String street;
+    private String zipcode;
+
+    // JPA 스펙상 만든 생성자
+    protected Address() {
+    }
+
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
+}
